372575f0928daa440b2d0113f7ddf3c248d5258f
Bianca Monteiro Corrêa <biancamonteiro22.06@gmail.com>
2021-09-13 19:02:54 -0300

    att da conexão com mongoDb

--- src/database/database.js
+++ src/database/database.js
@@ -1,20 +1,15 @@
-const express = require("express");
-const mongodb = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 require("dotenv").config();
 
-const connectToDb = () => {
-    const dbUser = process.env.DB_USER;
-	const dbPassword = process.env.DB_PASSWORD;
-	const dbName = process.env.DB_NAME;
-	const dbChar = process.env.DB_CHAR;
-	const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.${dbChar}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-	const options = {
-		useUnifiedTopology: true,
-	};
-	const client = await mongodb.MongoClient.connect(connectionString, options);
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.${process.env.DB_CHAR}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-    const db = client.db("akumanomi_db");
-    const akumasnomis = db.collection("akumasnomis");
-};
+const client = new MongoClient(url, {useUnifiedTopology: true, useNewUrlParser: true });
 
-module.exports = connectToDb;
+client.connect(() => {
+    console.log("Conectado Com Sucesso")
+});
+
+const db = client.db("onepiece_db");
+const akumanomi = db.collection('akumanomi');
+
+module.exports = {akumanomi,ObjectId};
