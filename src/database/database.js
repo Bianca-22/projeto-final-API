d2ed9147a3738a29fccc96abec92c72beb89c5b1
Bianca Monteiro Corrêa <83734803+Bianca-22@users.noreply.github.com>
2021-09-15 21:54:20 -0300

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -6,18 +6,4 @@ useNewUrlParser: true,
 useUnifiedTopology: true
 });
 
-/* const { MongoClient, ObjectId } = require("mongodb");
-require("dotenv").config();
-
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.${process.env.DB_CHAR}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-const client = new MongoClient(url, {useUnifiedTopology: true, useNewUrlParser: true });
-
-client.connect(() => {
-    console.log("Conectado Com Sucesso")
-});
-
-const db = client.db("onepiece_db");
-const Akumanomi = db.collection('akumanomi'); */
-
 module.exports = mongoose;
